11bb0a3e4d36eee46cbafb72671e9ed65970e6e6
MarinCervinschi <marincervinschi3@gmail.com>
2025-10-05 17:39:42 +0200

    feat(models): book, customer, reservation

---
+++ Models/Customer.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DotNetRestAPI.Models;
+
+public class Customer
+{
+    public int Id { get; set; }
+
+    [Required] [MaxLength(100)] public string FirstName { get; set; } = string.Empty;
+
+    [Required] [MaxLength(100)] public string LastName { get; set; } = string.Empty;
+
+    [Required]
+    [EmailAddress]
+    [MaxLength(255)]
+    public string Email { get; set; } = string.Empty;
+
+    // Navigation Properties
+    public List<Reservation> Reservations { get; set; } = new();
+}
