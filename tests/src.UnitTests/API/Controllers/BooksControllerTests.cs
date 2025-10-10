bd7b22590cdaadc1df238830632220945baf7ec5
MarinCervinschi <marincervinschi3@gmail.com>
2025-10-10 10:40:49 +0200

    feat: add unit tests for BooksController and BookService with builder pattern

---
+++ tests/src.UnitTests/API/Controllers/BooksControllerTests.cs
@@ -0,0 +1,389 @@
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using src.API.Controllers;
+using src.API.DTOs;
+using src.Core.Entities;
+using src.Core.Interfaces.Services;
+using src.UnitTests.Core.Builders;
+
+namespace src.UnitTests.API.Controllers;
+
+public class BooksControllerTests
+{
+    private readonly Mock<IBookService> _mockBookService;
+    private readonly BooksController _controller;
+
+    public BooksControllerTests()
+    {
+        _mockBookService = new Mock<IBookService>();
+        var mockLogger = new Mock<ILogger<BooksController>>();
+        _controller = new BooksController(_mockBookService.Object, mockLogger.Object);
+    }
+
+    [Fact]
+    public async Task GetAllBooks_ReturnsOkWithBookList()
+    {
+        // Arrange
+        var bookDtos = new List<BookDto>
+        {
+            BookBuilder.New().WithId(1).WithTitle("Book 1").WithIsbn("1111111111").BuildDto(),
+            BookBuilder.New().WithId(2).WithTitle("Book 2").WithIsbn("2222222222").BuildDto()
+        };
+
+        _mockBookService.Setup(s => s.GetAllBooksAsync())
+            .ReturnsAsync(bookDtos);
+
+        // Act
+        var result = await _controller.GetAllBooks();
+
+        // Assert
+        var okResult = result.Result.Should().BeOfType<OkObjectResult>().Subject;
+        okResult.StatusCode.Should().Be(200);
+
+        var returnedBooks = okResult.Value.Should().BeAssignableTo<IEnumerable<BookDto>>().Subject;
+        var bookList = returnedBooks.ToList();
+        bookList.Should().HaveCount(2);
+        bookList[0].Id.Should().Be(1);
+        bookList[0].Title.Should().Be("Book 1");
+        bookList[1].Id.Should().Be(2);
+        bookList[1].Title.Should().Be("Book 2");
+
+        _mockBookService.Verify(s => s.GetAllBooksAsync(), Times.Once);
+    }
+
+    [Fact]
+    public async Task GetAllBooks_WhenNoBooksExist_ReturnsOkWithEmptyList()
+    {
+        // Arrange
+        _mockBookService.Setup(s => s.GetAllBooksAsync())
+            .ReturnsAsync(new List<BookDto>());
+
+        // Act
+        var result = await _controller.GetAllBooks();
+
+        // Assert
+        var okResult = result.Result.Should().BeOfType<OkObjectResult>().Subject;
+        okResult.StatusCode.Should().Be(200);
+
+        var returnedBooks = okResult.Value.Should().BeAssignableTo<IEnumerable<BookDto>>().Subject;
+        returnedBooks.Should().BeEmpty();
+
+        _mockBookService.Verify(s => s.GetAllBooksAsync(), Times.Once);
+    }
+
+    [Fact]
+    public async Task GetBook_WithValidId_ReturnsOkWithBook()
+    {
+        // Arrange
+        var bookId = 1;
+        var bookDto = BookBuilder.New()
+            .WithId(bookId)
+            .WithTitle("Test Book")
+            .WithAuthor("Test Author")
+            .WithIsbn("1234567890")
+            .BuildDto();
+
+        _mockBookService.Setup(s => s.GetBookByIdAsync(bookId))
+            .ReturnsAsync(bookDto);
+
+        // Act
+        var result = await _controller.GetBook(bookId);
+
+        // Assert
+        var okResult = result.Result.Should().BeOfType<OkObjectResult>().Subject;
+        okResult.StatusCode.Should().Be(200);
+
+        var returnedBook = okResult.Value.Should().BeOfType<BookDto>().Subject;
+        returnedBook.Id.Should().Be(bookId);
+        returnedBook.Title.Should().Be("Test Book");
+        returnedBook.Author.Should().Be("Test Author");
+        returnedBook.ISBN.Should().Be("1234567890");
+
+        _mockBookService.Verify(s => s.GetBookByIdAsync(bookId), Times.Once);
+    }
+
+    [Fact]
+    public async Task GetBook_WithInvalidId_ReturnsNotFound()
+    {
+        // Arrange
+        var bookId = 999;
+        _mockBookService.Setup(s => s.GetBookByIdAsync(bookId))
+            .ReturnsAsync((BookDto?)null);
+
+        // Act
+        var result = await _controller.GetBook(bookId);
+
+        // Assert
+        result.Result.Should().BeOfType<NotFoundResult>();
+        var notFoundResult = result.Result as NotFoundResult;
+        notFoundResult!.StatusCode.Should().Be(404);
+
+        _mockBookService.Verify(s => s.GetBookByIdAsync(bookId), Times.Once);
+    }
+
+    [Fact]
+    public async Task CreateBook_WithValidData_ReturnsCreated()
+    {
+        // Arrange
+        var createDto = BookBuilder.New()
+            .WithTitle("New Book")
+            .WithAuthor("New Author")
+            .WithIsbn("9876543210")
+            .BuildCreateDto();
+
+        var createdBook = BookBuilder.New()
+            .WithId(1)
+            .WithTitle("New Book")
+            .WithAuthor("New Author")
+            .WithIsbn("9876543210")
+            .BuildDto();
+
+        _mockBookService.Setup(s => s.CreateBookAsync(createDto))
+            .ReturnsAsync(createdBook);
+
+        // Act
+        var result = await _controller.CreateBook(createDto);
+
+        // Assert
+        var createdResult = result.Result.Should().BeOfType<CreatedAtActionResult>().Subject;
+        createdResult.StatusCode.Should().Be(201);
+        createdResult.ActionName.Should().Be(nameof(BooksController.GetBook));
+        createdResult.RouteValues!["id"].Should().Be(1);
+
+        var returnedBook = createdResult.Value.Should().BeOfType<BookDto>().Subject;
+        returnedBook.Id.Should().Be(1);
+        returnedBook.Title.Should().Be("New Book");
+        returnedBook.Author.Should().Be("New Author");
+        returnedBook.ISBN.Should().Be("9876543210");
+
+        _mockBookService.Verify(s => s.CreateBookAsync(createDto), Times.Once);
+    }
+
+    [Fact]
+    public async Task CreateBook_WithInvalidModelState_ReturnsBadRequest()
+    {
+        // Arrange
+        var createDto = BookBuilder.New()
+            .WithTitle("")
+            .WithAuthor("")
+            .WithIsbn("invalid")
+            .BuildCreateDto();
+
+        _controller.ModelState.AddModelError("Title", "Title is required");
+        _controller.ModelState.AddModelError("Author", "Author is required");
+        _controller.ModelState.AddModelError("ISBN", "Invalid ISBN format");
+
+        // Act
+        var result = await _controller.CreateBook(createDto);
+
+        // Assert
+        var badRequestResult = result.Result.Should().BeOfType<BadRequestObjectResult>().Subject;
+        badRequestResult.StatusCode.Should().Be(400);
+        badRequestResult.Value.Should().BeOfType<SerializableError>();
+
+        _mockBookService.Verify(s => s.CreateBookAsync(It.IsAny<BookCreateDto>()), Times.Never);
+    }
+
+    [Fact]
+    public async Task CreateBook_WithDuplicateIsbn_ReturnsConflict()
+    {
+        // Arrange
+        var createDto = BookBuilder.New()
+            .WithTitle("New Book")
+            .WithAuthor("New Author")
+            .WithIsbn("1234567890")
+            .BuildCreateDto();
+
+        _mockBookService.Setup(s => s.CreateBookAsync(createDto))
+            .ThrowsAsync(new InvalidOperationException("A book with ISBN '1234567890' already exists."));
+
+        // Act
+        var result = await _controller.CreateBook(createDto);
+
+        // Assert
+        var conflictResult = result.Result.Should().BeOfType<ConflictObjectResult>().Subject;
+        conflictResult.StatusCode.Should().Be(409);
+
+
+        _mockBookService.Verify(s => s.CreateBookAsync(createDto), Times.Once);
+    }
+
+    [Fact]
+    public async Task CreateBook_WhenServiceThrowsException_ReturnsInternalServerError()
+    {
+        // Arrange
+        var createDto = BookBuilder.New()
+            .WithTitle("New Book")
+            .WithAuthor("New Author")
+            .WithIsbn("9876543210")
+            .BuildCreateDto();
+
+        _mockBookService.Setup(s => s.CreateBookAsync(createDto))
+            .ThrowsAsync(new Exception("Database error"));
+
+        // Act
+        var result = await _controller.CreateBook(createDto);
+
+        // Assert
+        var statusCodeResult = result.Result.Should().BeOfType<ObjectResult>().Subject;
+        statusCodeResult.StatusCode.Should().Be(500);
+        statusCodeResult.Value.Should().Be("Internal server error");
+
+        _mockBookService.Verify(s => s.CreateBookAsync(createDto), Times.Once);
+    }
+
+    [Fact]
+    public async Task UpdateBook_WithValidData_ReturnsOkWithUpdatedBook()
+    {
+        // Arrange
+        var bookId = 1;
+        var updateDto = BookBuilder.New()
+            .WithTitle("Updated Book")
+            .WithAuthor("Updated Author")
+            .WithIsbn("9999999999")
+            .WithUnavailableStatus()
+            .BuildUpdateDto();
+
+        var updatedBook = BookBuilder.New()
+            .WithId(bookId)
+            .WithTitle("Updated Book")
+            .WithAuthor("Updated Author")
+            .WithIsbn("9999999999")
+            .WithUnavailableStatus()
+            .BuildDto();
+
+        _mockBookService.Setup(s => s.UpdateBookAsync(bookId, updateDto))
+            .ReturnsAsync(updatedBook);
+
+        // Act
+        var result = await _controller.UpdateBook(bookId, updateDto);
+
+        // Assert
+        var okResult = result.Result.Should().BeOfType<OkObjectResult>().Subject;
+        okResult.StatusCode.Should().Be(200);
+
+        var returnedBook = okResult.Value.Should().BeOfType<BookDto>().Subject;
+        returnedBook.Id.Should().Be(bookId);
+        returnedBook.Title.Should().Be("Updated Book");
+        returnedBook.Author.Should().Be("Updated Author");
+        returnedBook.ISBN.Should().Be("9999999999");
+        returnedBook.Status.Should().Be(BookStatus.Unavailable);
+
+        _mockBookService.Verify(s => s.UpdateBookAsync(bookId, updateDto), Times.Once);
+    }
+
+    [Fact]
+    public async Task UpdateBook_WithInvalidModelState_ReturnsBadRequest()
+    {
+        // Arrange
+        var bookId = 1;
+        var updateDto = BookBuilder.New()
+            .WithTitle("")
+            .WithAuthor("")
+            .WithIsbn("invalid")
+            .BuildUpdateDto();
+
+        _controller.ModelState.AddModelError("Title", "Title is required");
+        _controller.ModelState.AddModelError("Author", "Author is required");
+        _controller.ModelState.AddModelError("ISBN", "Invalid ISBN format");
+
+        // Act
+        var result = await _controller.UpdateBook(bookId, updateDto);
+
+        // Assert
+        var badRequestResult = result.Result.Should().BeOfType<BadRequestObjectResult>().Subject;
+        badRequestResult.StatusCode.Should().Be(400);
+        badRequestResult.Value.Should().BeOfType<SerializableError>();
+
+        _mockBookService.Verify(s => s.UpdateBookAsync(It.IsAny<int>(), It.IsAny<BookUpdateDto>()), Times.Never);
+    }
+
+    [Fact]
+    public async Task UpdateBook_WhenBookNotExists_ReturnsNotFound()
+    {
+        // Arrange
+        var bookId = 999;
+        var updateDto = BookBuilder.New()
+            .WithTitle("Updated Book")
+            .WithAuthor("Updated Author")
+            .WithIsbn("9999999999")
+            .BuildUpdateDto();
+
+        _mockBookService.Setup(s => s.UpdateBookAsync(bookId, updateDto))
+            .ThrowsAsync(new KeyNotFoundException($"Book with ID {bookId} not found."));
+
+        // Act
+        var result = await _controller.UpdateBook(bookId, updateDto);
+
+        // Assert
+        result.Result.Should().BeOfType<NotFoundResult>();
+        var notFoundResult = result.Result as NotFoundResult;
+        notFoundResult!.StatusCode.Should().Be(404);
+
+        _mockBookService.Verify(s => s.UpdateBookAsync(bookId, updateDto), Times.Once);
+    }
+
+    [Fact]
+    public async Task UpdateBook_WithDuplicateIsbn_ReturnsConflict()
+    {
+        // Arrange
+        var bookId = 1;
+        var updateDto = BookBuilder.New()
+            .WithTitle("Updated Book")
+            .WithAuthor("Updated Author")
+            .WithIsbn("1234567890")
+            .BuildUpdateDto();
+
+        _mockBookService.Setup(s => s.UpdateBookAsync(bookId, updateDto))
+            .ThrowsAsync(new InvalidOperationException("A book with ISBN '1234567890' already exists."));
+
+        // Act
+        var result = await _controller.UpdateBook(bookId, updateDto);
+
+        // Assert
+        var conflictResult = result.Result.Should().BeOfType<ConflictObjectResult>().Subject;
+        conflictResult.StatusCode.Should().Be(409);
+
+        _mockBookService.Verify(s => s.UpdateBookAsync(bookId, updateDto), Times.Once);
+    }
+
+    [Fact]
+    public async Task DeleteBook_WhenBookExists_ReturnsNoContent()
+    {
+        // Arrange
+        var bookId = 1;
+        _mockBookService.Setup(s => s.DeleteBookAsync(bookId))
+            .ReturnsAsync(true);
+
+        // Act
+        var result = await _controller.DeleteBook(bookId);
+
+        // Assert
+        result.Should().BeOfType<NoContentResult>();
+        var noContentResult = result as NoContentResult;
+        noContentResult!.StatusCode.Should().Be(204);
+
+        _mockBookService.Verify(s => s.DeleteBookAsync(bookId), Times.Once);
+    }
+
+    [Fact]
+    public async Task DeleteBook_WhenBookNotExists_ReturnsNotFound()
+    {
+        // Arrange
+        var bookId = 999;
+        _mockBookService.Setup(s => s.DeleteBookAsync(bookId))
+            .ReturnsAsync(false);
+
+        // Act
+        var result = await _controller.DeleteBook(bookId);
+
+        // Assert
+        result.Should().BeOfType<NotFoundResult>();
+        var notFoundResult = result as NotFoundResult;
+        notFoundResult!.StatusCode.Should().Be(404);
+
+        _mockBookService.Verify(s => s.DeleteBookAsync(bookId), Times.Once);
+    }
+}
---
+++ tests/src.UnitTests/Core/Builders/BookBuilder.cs
@@ -0,0 +1,117 @@
+using src.API.DTOs;
+using src.Core.Entities;
+
+namespace src.UnitTests.Core.Builders;
+
+public class BookBuilder
+{
+    private int _id = 1;
+    private string _title = "Default Title";
+    private string _author = "Default Author";
+    private string _isbn = "1234567890";
+    private BookStatus _status = BookStatus.Available;
+
+    public BookBuilder WithId(int id)
+    {
+        _id = id;
+        return this;
+    }
+
+    public BookBuilder WithTitle(string title)
+    {
+        _title = title;
+        return this;
+    }
+
+    public BookBuilder WithAuthor(string author)
+    {
+        _author = author;
+        return this;
+    }
+
+    public BookBuilder WithIsbn(string isbn)
+    {
+        _isbn = isbn;
+        return this;
+    }
+
+    public BookBuilder WithStatus(BookStatus status)
+    {
+        _status = status;
+        return this;
+    }
+
+    public BookBuilder WithAvailableStatus()
+    {
+        _status = BookStatus.Available;
+        return this;
+    }
+
+    public BookBuilder WithUnavailableStatus()
+    {
+        _status = BookStatus.Unavailable;
+        return this;
+    }
+
+    public Book Build()
+    {
+        return new Book
+        {
+            Id = _id,
+            Title = _title,
+            Author = _author,
+            ISBN = _isbn,
+            Status = _status
+        };
+    }
+
+    public BookDto BuildDto()
+    {
+        return new BookDto
+        {
+            Id = _id,
+            Title = _title,
+            Author = _author,
+            ISBN = _isbn,
+            Status = _status
+        };
+    }
+
+    public BookCreateDto BuildCreateDto()
+    {
+        return new BookCreateDto
+        {
+            Title = _title,
+            Author = _author,
+            ISBN = _isbn,
+            Status = _status
+        };
+    }
+
+    public BookUpdateDto BuildUpdateDto()
+    {
+        return new BookUpdateDto
+        {
+            Title = _title,
+            Author = _author,
+            ISBN = _isbn,
+            Status = _status
+        };
+    }
+
+    public static BookBuilder New() => new();
+
+    public static BookBuilder ABook() => new();
+
+    public static BookBuilder ABookWithTitle(string title) => new BookBuilder().WithTitle(title);
+
+    public static BookBuilder ABookWithIsbn(string isbn) => new BookBuilder().WithIsbn(isbn);
+
+    public static BookBuilder AnUnavailableBook() => new BookBuilder().WithUnavailableStatus();
+
+    public static BookBuilder AValidBook() => new BookBuilder()
+        .WithTitle("The Great Gatsby")
+        .WithAuthor("F. Scott Fitzgerald")
+        .WithIsbn("9780743273565")
+        .WithAvailableStatus();
+}
